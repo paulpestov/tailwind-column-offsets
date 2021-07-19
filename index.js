4931c7a9e4a78965ad9cf6c80a02b0896bfc08c2
Paul Pestov <pestov@sub.uni-goettingen.de>
2021-07-19 21:30:04 +0200

    Add files

---
+++ index.js
@@ -0,0 +1,39 @@
+module.exports = function () {
+  return function({ addUtilities }) {
+    addUtilities({
+      '.offset-1': {
+        marginLeft: '8.33333333%'
+      },
+      '.offset-2': {
+        marginLeft: '16.66666%'
+      },
+      '.offset-3': {
+        marginLeft: '25%'
+      },
+      '.offset-4': {
+        marginLeft: '33.33333333%'
+      },
+      '.offset-5': {
+        marginLeft: '41.66666667%'
+      },
+      '.offset-6': {
+        marginLeft: '50%'
+      },
+      '.offset-7': {
+        marginLeft: '58.33333333%'
+      },
+      '.offset-8': {
+        marginLeft: '66.66666667%'
+      },
+      '.offset-9': {
+        marginLeft: '75%'
+      },
+      '.offset-10': {
+        marginLeft: '83.33333333%'
+      },
+      '.offset-11': {
+        marginLeft: '91.66666667%'
+      },
+    })
+  }
+}
